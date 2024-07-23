5750bbcb0cde0298a691b8558c3e3981a5526bb9
Burdy <burdy.goureige.ep@whoz.com>
2024-07-23 09:43:23 +0200

    initialization commit

---
+++ src/main/java/com/devEntrepreneurFormation/ensembleMamanApi/EnsembleMamanApiApplication.java
@@ -0,0 +1,13 @@
+package com.devEntrepreneurFormation.ensembleMamanApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsembleMamanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsembleMamanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devEntrepreneurFormation/ensembleMamanApi/EnsembleMamanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devEntrepreneurFormation.ensembleMamanApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsembleMamanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
